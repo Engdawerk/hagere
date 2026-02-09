ee91f9b76fa885c02fca2c03a6e861825fd73461
Engdawerk <116351980+Engdawerk@users.noreply.github.com>
2026-02-09 10:49:19 +0300

    Update SERVER.js

--- BACKEND/SERVER.js
+++ BACKEND/SERVER.js
@@ -90,7 +90,7 @@ initDB().then(() => {
     //app.listen(PORT, () => {
         //console.log("Server is running on port " + PORT);
    // });
-    app.listen(port, '0.0.0.0', () => {
+    app.listen(PORT, '0.0.0.0', () => {
        console.log(`Server running on port ${port}`);
     });
 }
